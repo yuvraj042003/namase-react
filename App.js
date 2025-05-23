de6cc2f47c61788ff3020a856275cbcb8f50672e
yuvraj042003 <yuvraj042003@gmail.com>
2025-05-23 18:20:05 +0530

    L3: Changes in App.js File

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { createRoot } from "react-dom/client"; 
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { key: "h1" }, "This is h1 Tag"),
-    React.createElement("h2", { key: "h2" }, "This is h2 Tag"),
-  ])
-);
+// React Elelment : WHEN I CREATE A DOM ELELMENT, This is a react-element. 
+// React.createElement ==> Object ==> HTTPElement(render);
+
+
+const heading = React.createElement("h1", {}, "Hello This is aman Friernd Yuvraj Singh");
+
+const jsxHeading = <h1 id="name">Namaste Yuvi! using JSX. </h1>
+console.log("headng--->>>", heading);
+console.log("JSX--->>", jsxHeading);
+
+// React Functional Componenet Element
+
+const number = 576876;
+const HeadingComponent = () => {
+  return <h1>This is my functional component</h1>
+}
+// React Elelment
+
+const AnotherComponent = () => (
+ 
+  <div className="IdS">
+   {number}
+  <HeadingComponent />
+  <h1>This is another component</h1>
+
+  </div>
+)
 
 const root = createRoot(document.getElementById("root"));
-root.render(parent);
+
+// JSX (Jsx transpiled before if reaches JS Engine).. -->>> It dones by Parcel.. --->> It's done by parcel.  
+
+root.render(<AnotherComponent />)
