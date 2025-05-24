fdcdf4632849e582435c6dbeac3e0212e914c2d0
yuvraj042003 <yuvraj042003@gmail.com>
2025-05-24 18:41:21 +0530

    L4: Make a Resto Cart Component

--- App.js
+++ App.js
@@ -3,34 +3,91 @@ import { createRoot } from "react-dom/client";
 
 // React Elelment : WHEN I CREATE A DOM ELELMENT, This is a react-element. 
 // React.createElement ==> Object ==> HTTPElement(render);
+// 
 
+// 
 
-const heading = React.createElement("h1", {}, "Hello This is aman Friernd Yuvraj Singh");
+// 
+/**
+ * Home
+ *  -Logo
+ * -Nav Items
+ * Body
+ *  -->> Search
+ *  -->> Cart Container
+ *  -->> RestaurantCaontainer
+ *    -->>>>> RestaurantCar
+ * Footer
+ *  -->> Copyright
+ * -->> Links
+ * -->>Address
+ * -->>Content 
+ */
+// 
 
-const jsxHeading = <h1 id="name">Namaste Yuvi! using JSX. </h1>
-console.log("headng--->>>", heading);
-console.log("JSX--->>", jsxHeading);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img  className="logo" src="https://static.vecteezy.com/system/resources/thumbnails/005/513/590/small/catering-quality-food-design-premium-logo-vector.jpg" alt="App Logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+            <li>Home</li>
+            <li>Restaurant</li>
+            <li>Foods</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+        </ul>
 
-// React Functional Componenet Element
-
-const number = 576876;
-const HeadingComponent = () => {
-  return <h1>This is my functional component</h1>
+      </div>
+    </div>
+  )
 }
-// React Elelment
+// Restro-Cart
+const RestroCart = () => {
+  return (
+    <div className="res-card">
+    <img src="
+      https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/4/9/b7f823ab-9291-4be3-bb39-e5415ca70c35_5a6a3c4d-1a35-44c7-b7f0-02fd55b9fc83.JPG
+    "
+      className="food-img"
+    />
+    <div className="res-items">
 
-const AnotherComponent = () => (
- 
-  <div className="IdS">
-   {number}
-  <HeadingComponent />
-  <h1>This is another component</h1>
+      <h3> Veg-Biryani 😋</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 Stars</h4>
+      <h4>27 minutes</h4>
+    </div>
+    </div>
+  )
+}
 
-  </div>
-)
+// Body:::
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        {/* Res-component */}
+        <RestroCart />
+      </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  )
+}
 
 const root = createRoot(document.getElementById("root"));
 
 // JSX (Jsx transpiled before if reaches JS Engine).. -->>> It dones by Parcel.. --->> It's done by parcel.  
 
-root.render(<AnotherComponent />)
+root.render(<AppLayout />)
